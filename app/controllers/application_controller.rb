7f59bd97725c33c3109bf1881113bc207700c620
Rémy Hannequin <remy.hannequin@gmail.com>
2014-04-02 17:39:28 +0200

    Handle page params hash from controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :set_locale
+  before_action :set_locale, :init_page_params
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, alert: exception.message
@@ -12,5 +12,15 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = I18n.default_locale
   end
+
+  def set_page_params(obj)
+    @page_params = @page_params.merge obj
+  end
+
+  private
+    def init_page_params
+      @page_params = {}
+      set_page_params foursquare: { oauth_token: ENV['FOURSQUARE_TOKEN'] }
+    end
 
 end
