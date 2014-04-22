6ab3ae563715e055d68d6388bb36e4c2d8fe31b1
Rémy Hannequin <remy.hannequin@gmail.com>
2014-04-22 15:02:35 +0200

    Add landing page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
 class HomeController < ApplicationController
+  layout :custom_layout
+
+  def index
+  end
+
+  def main
+  end
+
+  private
+    def custom_layout
+      action_name != 'index' ? 'application': false
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Meetformeal::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.assets.raise_production_errors = true
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
