1e0dee8f17661874f4be85d39d59c6e13f34a689
Rémy Hannequin <remy.hannequin@gmail.com>
2014-04-28 08:23:51 +0200

    Use factories for model User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,102 +2,91 @@ require 'spec_helper'
 
 describe User do
 
-  before(:each) do
-    @attr = {
-      :name => "Example User",
-      :email => "user@example.com",
-      :password => "changeme",
-      :password_confirmation => "changeme"
-    }
+  it 'should create a new instance given a valid attribute' do
+    FactoryGirl.create :user
   end
 
-  it "should create a new instance given a valid attribute" do
-    User.create!(@attr)
-  end
+  describe 'email' do
+    it 'should be required' do
+      no_email_user = FactoryGirl.build :user, email: ''
+      no_email_user.should_not be_valid
+    end
 
-  it "should require an email address" do
-    no_email_user = User.new(@attr.merge(:email => ""))
-    no_email_user.should_not be_valid
-  end
+    it 'should be accepted when valid' do
+      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+      addresses.each do |address|
+        valid_email_user = FactoryGirl.build :user, email: address
+        valid_email_user.should be_valid
+      end
+    end
 
-  it "should accept valid email addresses" do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-    addresses.each do |address|
-      valid_email_user = User.new(@attr.merge(:email => address))
-      valid_email_user.should be_valid
+    it 'should be rejected when not valid' do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses.each do |address|
+        invalid_email_user = FactoryGirl.build :user, email: address
+        invalid_email_user.should_not be_valid
+      end
+    end
+
+    it 'should be rejected when duplicated' do
+      first_user = FactoryGirl.create :user
+      user_with_duplicate_email = FactoryGirl.build :user, email: first_user.email
+      user_with_duplicate_email.should_not be_valid
+    end
+
+    it 'should be rejected when identical up to case' do
+      user = FactoryGirl.create :user
+      user_with_duplicate_email = FactoryGirl.build :user, email: user.email.upcase
+      user_with_duplicate_email.should_not be_valid
     end
   end
 
-  it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-    addresses.each do |address|
-      invalid_email_user = User.new(@attr.merge(:email => address))
-      invalid_email_user.should_not be_valid
-    end
-  end
-
-  it "should reject duplicate email addresses" do
-    User.create!(@attr)
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
-
-  it "should reject email addresses identical up to case" do
-    upcased_email = @attr[:email].upcase
-    User.create!(@attr.merge(:email => upcased_email))
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
-
-  describe "passwords" do
-
+  describe 'passwords' do
     before(:each) do
-      @user = User.new(@attr)
+      @user = FactoryGirl.build :user
     end
 
-    it "should have a password attribute" do
+    it 'should have a password attribute' do
       @user.should respond_to(:password)
     end
 
-    it "should have a password confirmation attribute" do
+    it 'should have a password confirmation attribute' do
       @user.should respond_to(:password_confirmation)
     end
   end
 
-  describe "password validations" do
-
-    it "should require a password" do
-      User.new(@attr.merge(:password => "", :password_confirmation => "")).
-        should_not be_valid
+  describe 'password validations' do
+    it 'should require a password' do
+      FactoryGirl.build(:user, password: '', password_confirmation: '')
+        .should_not be_valid
     end
 
-    it "should require a matching password confirmation" do
-      User.new(@attr.merge(:password_confirmation => "invalid")).
-        should_not be_valid
+    it 'should require a matching password confirmation' do
+      FactoryGirl.build(:user, password_confirmation: 'invalid').should_not be_valid
     end
 
-    it "should reject short passwords" do
-      short = "a" * 5
-      hash = @attr.merge(:password => short, :password_confirmation => short)
-      User.new(hash).should_not be_valid
+    it 'should reject short passwords' do
+      short = 'a' * 5
+      FactoryGirl.build(:user, password: short, password_confirmation: short)
+        .should_not be_valid
     end
 
   end
 
-  describe "password encryption" do
-
+  describe 'password encryption' do
     before(:each) do
-      @user = User.create!(@attr)
+      @user = FactoryGirl.create :user
     end
 
-    it "should have an encrypted password attribute" do
+    it 'should have an encrypted password attribute' do
       @user.should respond_to(:encrypted_password)
     end
 
-    it "should set the encrypted password attribute" do
+    it 'should set the encrypted password attribute' do
       @user.encrypted_password.should_not be_blank
     end
 
   end
 
 end
+
