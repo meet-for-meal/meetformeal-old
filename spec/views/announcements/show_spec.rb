597cff060e2c8b4d244ba0e46fc95f306dac001b
Rémy Hannequin <remy.hannequin@gmail.com>
2014-05-14 16:44:26 +0200

    Display user name when showing announcement

--- spec/views/announcements/show_spec.rb
+++ spec/views/announcements/show_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 describe 'announcements/show' do
-  let(:announcement) { create(:announcement) }
+  let(:announcement) { create(:announcement_with_user) }
+
+  it 'displays the user name' do
+    assign(:announcement, announcement)
+    render
+    expect(rendered).to include(announcement.user.name)
+  end
 
   it 'displays the announcement location' do
     assign(:announcement, announcement)
