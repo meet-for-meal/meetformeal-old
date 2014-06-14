8427dff852e44981c422701775f2c653cd2246c7
Rémy Hannequin <remy.hannequin@gmail.com>
2014-06-14 16:53:10 +0200

    User edit/new registration design

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@ Rails.application.config.assets.precompile += [
   'home.js',
   'home.css',
   'registrations.js',
+  'registrations.css',
   'users.js',
   'announcements.js',
   'announcements.css',
